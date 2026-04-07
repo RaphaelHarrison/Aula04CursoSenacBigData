6c20581c84a90582ea0b489e114a0976dec6c754
RaphaelHarrison <raphaelsantos229@gmail.com>
2026-04-07 20:47:58 -0300

    Atualizando atividade01

--- aula04/atividade01.py
+++ aula04/atividade01.py
@@ -1,7 +1,7 @@
 # realizar um programa que sempre que o cliente realizar uma compra acima de R$ 250,00, o sistema deve aplicar 16% de desconto. Caso contário, o valor da compra deve permanecer o mesmo
 
 valor = float(input("Entre com o valor da compra: "))
-if valor >= 250:
+if valor > 250:
     novo_valor = valor - (valor * 0.16)
     print(f"O novo valor é {novo_valor}")
 else:
